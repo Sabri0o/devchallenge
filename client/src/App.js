df8286572e1f0ce662f246f747900663d3c5b3ad
Sabri0o <sabritrabelsi90@gmail.com>
2021-01-19 18:59:13 +0100

    setting environement

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from '../src/components/navbar/header'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ client/src/components/navbar/header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+import "./testing.header.scss"
+
+export default function Header() {
+    return (
+        <div>
+  <Navbar >
+    <Form inline>
+      <Navbar.Brand>
+          <img
+            src="https://inconclusive-clock.surge.sh/static/media/my_unsplash_logo.e948d53e.svg"
+            width="140"
+            height="30"/>
+      </Navbar.Brand>
+      <FormControl  type="text" placeholder="Search"  />
+      <Button className="toggle-nav" >Add a photo</Button>
+    </Form>
+    
+  </Navbar>
+        </div>
+    )
+}
