606310a320a3b513e7ec065ca367e57fbac24205
Sabri0o <sabritrabelsi90@gmail.com>
2021-01-20 14:36:04 +0100

    css styling test

--- client/src/components/imageCard/imagecard.js
+++ client/src/components/imageCard/imagecard.js
@@ -1,17 +1,47 @@
 import React from "react";
+import Card from 'react-bootstrap/Card';
 
 
+// class ImageCard extends React.Component {
+//     render() {
+//       return (
+//         <div>
+//           <img
+//             src={this.props.image}
+//             alt='test'
+//           />
+//         </div>
+//       );
+//     }
+//   }
 class ImageCard extends React.Component {
-    render() {
-      return (
-        <div>
-          <img
-            src={this.props.image}
-            alt='test'
-          />
-        </div>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {spans:0};
+    this.imageRef = React.createRef();
   }
 
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10 );
+    this.setState({ spans: spans});
+  }
+
+  render() {
+   // const { description, urls } = this.props.image;
+
+    return (
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <Card.Img variant="top" src={this.props.image}  ref={this.imageRef} />
+        
+      </div>
+    );
+  }
+}
+
 export default ImageCard;
+/*<img ref={this.imageRef} src={this.props.image} alt='blabla' />*/
