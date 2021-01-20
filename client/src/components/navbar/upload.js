f2f6b8f464eeff1070978931cbf1a27c668adeb4
Sabri0o <sabritrabelsi90@gmail.com>
2021-01-20 22:04:43 +0100

    customize pop up

--- client/src/components/navbar/upload.js
+++ client/src/components/navbar/upload.js
@@ -1,20 +1,37 @@
-//import Modal from 'react-bootstrap/Modal';
-//import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 import React from "react";
 
-import './upload.css';
+
 
 const Upload = (props) => {
-  
-    const showHideClassName = props.show ? "modal display-block" : "modal display-none";
 
-    return (
-      <div className={showHideClassName}>
-        <section className="modal-main">
-          <button onClick={props.handleClose}>Close</button>
-        </section>
-      </div>
-    );
-  };
+return (
+    <Modal
+    show={props.show} onHide={props.handleClose}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Add photo
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.handleClose}>Cancel</Button>
+        <Button >Upload</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
 
   export default Upload;
